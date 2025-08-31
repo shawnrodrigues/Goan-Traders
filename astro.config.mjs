0cfeaf67039e7ff9437182778a048dc5145a7507
Shawn Rodrigues <shawn.rodrigues3308@gmail.com>
2025-08-31 18:54:55 +0530

    Vercel update

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,19 +3,19 @@ import dotenv from 'dotenv';
 
 import node from '@astrojs/node';
 
+import vercel from '@astrojs/vercel';
+
 dotenv.config();
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://deft-syrniki-7ef652..app',
+  site: 'https://goantraders.com',
   compressHTML: true,
 
   build: {
     inlineStylesheets: 'auto',
   },
 
-  adapter: node({
-    mode: 'standalone'
-  })
+  adapter: vercel()
 });
 // This file is used to configure the Astro project, including environment variables.
